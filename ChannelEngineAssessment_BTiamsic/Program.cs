3a5e84b19e95c7b9f2b2c2e8230912aa0afff29f
bjaytiamsic26 <36691063+bjaytiamsic26@users.noreply.github.com>
2022-03-30 18:58:26 +0800

    Add project files.

---
+++ ChannelEngineAssessment_BTiamsic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChannelEngineAssessment_BTiamsic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
